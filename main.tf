6ed48811f1054ed6c5edc49379ba2f6feb13e5a0
ambesteves <amb.esteves@gmail.com>
2023-03-13 15:51:51 +0000

    Use the self variable

--- main.tf
+++ main.tf
@@ -6,3 +6,14 @@ resource "aws_instance" "instance_1" {
   }
 }
 
+resource "aws_instance" "instance_2" {
+  ami           = var.instance_2_ami
+  instance_type = var.instance_2_type
+  tags = {
+    Name = var.instance_2_name
+  }
+  provisioner "local-exec" {
+    command    = "echo The IP address of the Server is ${self.private_ip}"
+    on_failure = continue
+  }
+}
